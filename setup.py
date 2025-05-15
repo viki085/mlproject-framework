1ea7e91e8f75050f0066fdac818810ee0e9af038
Vikram Karthikeyanl <viki.kma085@gmail.com>
2025-05-15 12:00:04 +0530

    'Setup'

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirenments(file_path: str) -> List[str]:
+    '''
+    This function will return a list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Vikram Karthikeyan',
+    author_email='viki.kma085@gmaiol.com',
+    packages=find_packages(),
+    install_requires=get_requirenments('requirements.txt')
+)
